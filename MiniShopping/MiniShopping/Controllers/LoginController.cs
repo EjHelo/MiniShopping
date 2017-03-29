8fbf6c722931137fe979b26760f9760bb4a04759
jodaniel <jodaniel94@gmail.com>
2017-03-29 15:35:25 -0600

    Login controller

---
+++ MiniShopping/MiniShopping/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Neo4j.Driver.V1;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MiniShopping.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void Neo4jConnector()
+        {
+            using (var driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "neo4j")))
+            using (var session = driver.Session())
+            {
+                //session.Run()
+            }
+        }
+    }
+}
